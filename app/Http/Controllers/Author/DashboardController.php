ebddc064ada0d4901008224ea806b19d46801ca4
omarkawser <60486575+omarkawser@users.noreply.github.com>
2020-04-10 10:33:40 +0600

    Multi auth

---
+++ app/Http/Controllers/Author/DashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Author;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DashboardController extends Controller
+{
+    public function index(){
+    
+        return view ('admin.dashboard' );
+        
+        
+    
+
+    }
+}
